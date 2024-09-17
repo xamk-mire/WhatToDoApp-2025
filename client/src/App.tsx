d01ffb01b47f990f0abf2583c61f4238441a9f61
Bit <bitlessniper@gmail.com>
2024-09-17 18:59:21 +0300

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <button className="btn btn-primary">Test</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
